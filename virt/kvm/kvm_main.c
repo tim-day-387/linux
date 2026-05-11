bf3a36f0d75f817e796264b3f5882bd74fe6fdab
Timothy Day <timday@thelustrecollective.com>
2026-05-11 12:28:00 -0400

    Make /dev/kvm 666 and S_IMMUTABLE

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -49,6 +49,7 @@
 #include <linux/lockdep.h>
 #include <linux/kthread.h>
 #include <linux/suspend.h>
+#include <linux/namei.h>
 
 #include <asm/processor.h>
 #include <asm/ioctl.h>
@@ -5564,7 +5565,21 @@ static struct miscdevice kvm_dev = {
 	KVM_MINOR,
 	"kvm",
 	&kvm_chardev_ops,
+	.mode = 0666,
 };
+
+static void kvm_set_dev_immutable(bool immutable)
+{
+	struct path path;
+
+	if (kern_path("/dev/kvm", LOOKUP_FOLLOW, &path))
+		return;
+	if (immutable)
+		path.dentry->d_inode->i_flags |= S_IMMUTABLE;
+	else
+		path.dentry->d_inode->i_flags &= ~S_IMMUTABLE;
+	path_put(&path);
+}
 
 #ifdef CONFIG_KVM_GENERIC_HARDWARE_ENABLING
 bool enable_virt_at_load = true;
@@ -6533,6 +6548,8 @@ int kvm_init(unsigned vcpu_size, unsigned vcpu_align, struct module *module)
 		goto err_register;
 	}
 
+	kvm_set_dev_immutable(true);
+
 	return 0;
 
 err_register:
@@ -6561,6 +6578,7 @@ void kvm_exit(void)
 	 * fops_get(), a.k.a. try_module_get(), prevents acquiring references
 	 * to KVM while the module is being stopped.
 	 */
+	kvm_set_dev_immutable(false);
 	misc_deregister(&kvm_dev);
 
 	kvm_uninit_virtualization();
